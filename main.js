d7aaa07813956fb07a66313db0d65b5c825c8d5f
ashishraj2024 <138851362+ashishraj2024@users.noreply.github.com>
2024-02-25 17:32:38 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed= new Typed(".text", {
-    strings:  ["FullStack Developer" , "Salesforce Developer" , "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
